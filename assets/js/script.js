c5db20f4c1272a6e933bb45f5807a9972de2df33
LilianaAbreo <abreomanosalva4@gmail.com>
2023-03-16 14:42:29 -0300

    Se agregó en el script.js un comando para esconder el texto de quiénes somos  y dejar solo los favicons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,7 @@ $(document).ready(function () {
   $("#correo").click(function () {
     alert("Fue enviado correctamente"); // Se ha enviado correctamente
   });
-
+  $("div div.fondo_iconos").click(function () {
+    $(" div p").toggle();
+  });
 });
