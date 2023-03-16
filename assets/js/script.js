f59b54449e13f0fe855e604537ace410d09441a1
LilianaAbreo <abreomanosalva4@gmail.com>
2023-03-16 04:05:30 -0300

    Últimos ajustes en el nav del archivo index.html, edición en el archivo script.js y style.css.  Igualmente se hizo la identación de todo el proyecto.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,4 @@ $(document).ready(function () {
     alert("Fue enviado correctamente"); // Se ha enviado correctamente
   });
 
-  $("h6").on("dblclick", function () {
-    $(this).css({
-      "color": "red"
-    })
-  });
-
-
-  $("div h5.card-title").click(function () {
-    $(" div p.card-text").toggle();
-  });
-
 });
